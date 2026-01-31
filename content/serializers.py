6fbb53b8c2ccfb8fc312b5e9b26750210eaca168
MarekSvec <marek.svec.das@gmail.com>
2026-01-31 13:13:50 +0100

    soubory a složky funguji, dolni lišta

--- content/serializers.py
+++ content/serializers.py
@@ -14,7 +14,7 @@ class DesktopFileSerializer(serializers.ModelSerializer):
         model = DesktopFile
         fields = ['id', 'name', 'file_type', 'folder', 'content', 'file_size', 
                   'created_at', 'modified_at', 'x_position', 'y_position', 'icon_color']
-        read_only_fields = ['created_at', 'modified_at']
+        read_only_fields = ['created_at', 'modified_at', 'file_size']
 
 
 class DesktopWindowSerializer(serializers.ModelSerializer):
